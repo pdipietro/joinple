ff8e06e572a62d48943b8fa80e92f53db105c185
Neo4j-Ubuntu64 <paolodipietro58@gmail.com>
2014-11-11 12:11:14 +0100

    checkout simple_token_authentication

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gsnApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gsnApp
+ */
+angular.module('aGsn.controller')
+  .controller('LoginCtrl', function ($scope) {
+
+  });
