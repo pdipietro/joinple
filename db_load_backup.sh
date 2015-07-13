83593caaa4fe195af881e7977bb80084b366fd7d
Neo4j-Ubuntu64 <paolodipietro58@gmail.com>
2015-07-13 13:06:13 +0200

    drive for backup restore db

---
+++ db_load_backup.sh
@@ -0,0 +1,5 @@
+# set -vx
+cd $HOME/gsn
+
+sudo cat ./db/dev_backup.txt | ./db/neo4j/development/bin/neo4j-shell neo4j.properties -path ./db/neo4j/development/data/graph.db > ./db/neo4j/development/data/log/db_load.log   2>./db/neo4j/development/data/log/db_load_stderr.log
+
